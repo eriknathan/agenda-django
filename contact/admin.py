e82951c263d4c931ef36199b8638c998cb965004
eriknathan <eriknathan.contato@gmail.com>
2023-09-20 20:26:27 -0300

    feat: adicionando ImageField e configurando o MEDIA_URL e MEDIA_ROOT

--- contact/admin.py
+++ contact/admin.py
@@ -5,11 +5,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmim(admin.ModelAdmin):
-    list_display = 'id', 'fisrt_name', 'last_name', 'phone', 'email', 'created_date',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 'created_date',
     ordering = 'id',
     # list_filter = 'created_date',
-    search_fields = 'id', 'fisrt_name', 'last_name',
+    search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
-    # list_editable = 'fisrt_name', 'last_name',
+    # list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
--- contact/models.py
+++ contact/models.py
@@ -3,12 +3,14 @@ from django.utils import timezone
 
 
 class Contact(models.Model):
-    fisrt_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self) -> str:
-        return f'{self.fisrt_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
