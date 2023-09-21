5ed5cbf28cc95bb6b7c771e77049863e611c3eef
eriknathan <eriknathan.contato@gmail.com>
2023-09-21 10:09:57 -0300

    feat: adicionando paginação e algumas templates base

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,60 @@
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
+from django.core.paginator import Paginator
 from contact.models import Contact
 
 
 def index(request):
     contacts = Contact.objects \
         .filter(show=True) \
-        .order_by('-id')[:10]
+        .order_by('-id')
 
-    print(contacts.query)
+    # print(contacts.query)
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
-    context = {'contacts': contacts}
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Contatos - '
+    }
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    # print('search_value',search_value)
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(id__icontains=search_value)
+        ) \
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    # print(contacts.query)
+
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Search - '
+    }
+
     return render(
         request,
         'contact/index.html',
@@ -21,7 +65,12 @@ def index(request):
 def contact(request, contact_id):
     single_contacts = get_object_or_404(Contact, pk=contact_id, show=True)
 
-    context = {'contact': single_contacts}
+    site_title = f'{single_contacts.first_name} {single_contacts.last_name} - '
+
+    context = {
+        'contact': single_contacts,
+        'site_title': site_title   
+    }
 
     return render(
         request,
