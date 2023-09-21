0c4c3d60a65338734a8be2be61a161ad4d55259a
eriknathan <eriknathan.contato@gmail.com>
2023-09-20 22:49:33 -0300

    feat: criando o template, a view e a url para exibir as pessoas

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from contact.models import Contact
 
 
-# Create your views here.
 def index(request):
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .order_by('-id')[:10]
+
+    print(contacts.query)
+
+    context = {'contacts': contacts}
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    single_contacts = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    context = {'contact': single_contacts}
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
     )
