f1819554b092bcd093513201055fc36e5598718f
eriknathan <eriknathan.contato@gmail.com>
2023-09-25 10:54:41 -0300

    feat: criando tela de login e logout

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 from contact.forms import RegisterForm
-
+from django.contrib import auth
 
 def register(request):
     form = RegisterForm()
@@ -17,7 +18,7 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Usuário Registrado')
-            return redirect('contact:index')
+            return redirect('contact:login')
     
     return render(
         request,
@@ -26,3 +27,29 @@ def register(request):
             'form': form
         }
     )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        
+        if form.is_valid():
+            user = form.get_user()
+            messages.success(request, 'Logado com sucesso!')
+            auth.login(request, user)
+            return redirect('contact:index')
+        messages.error(request, 'Login inválido!')
+    
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+    
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
